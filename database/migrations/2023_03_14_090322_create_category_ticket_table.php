6dc6958e9dc94a52ca3bba991af5b5c7d2923843
Mohammed Altohami <altohami360@gmail.com>
2025-07-01 22:57:39 +0300

    add id

--- database/migrations/2023_03_14_090322_create_category_ticket_table.php
+++ database/migrations/2023_03_14_090322_create_category_ticket_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('category_ticket', function (Blueprint $table) {
+            $table->id();
             $table->foreignId('category_id')->constrained()->cascadeOnDelete();
             $table->foreignId('ticket_id')->constrained()->cascadeOnDelete();
         });
